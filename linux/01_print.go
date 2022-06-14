59b8e9888d1e168ff304f1a07b419c9d1f8a85dc
apurugganan <apurugganan1@my.bcit.ca>
2022-06-14 09:30:05 -0700

    string indexing

--- linux/01_print.go
+++ linux/01_print.go
@@ -6,6 +6,11 @@ func main() {
 	name := "bob"
 	fmt.Println("Hello World")
 	fmt.Printf("The name is %v\n", name)
+
 	capture := fmt.Sprintf("string capture")
-	fmt.Println(capture)
+	fmt.Println(capture[0:1])       // prints s
+	fmt.Println(capture[0])         // prints 115; ascii value
+	fmt.Println(string(capture[0])) // prints s; string converts ascii
+	fmt.Println("hello"[1])         // prints 101
+
 }
